f18c5cd9db296d973d464e86765cab7746be958e
Ilyes Souaidia <leilyesdu06300@gmail.com>
2024-07-26 12:59:17 +0200

    ex01

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -5,9 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: isouaidi <isouaidi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/22 16:59:07 by isouaidi          #+#    #+#             */
-/*   Updated: 2024/07/22 16:59:14 by isouaidi         ###   ########.fr       */
+/*   Created: 2024/07/23 19:27:52 by isouaidi          #+#    #+#             */
+/*   Updated: 2024/07/23 19:29:25 by isouaidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <algorithm>
+#include <iterator>
+#include <exception>
+#include <vector>
+#include <iostream>
+
+
+class notfound : public std ::exception
+{
+    public:
+    	virtual const char* what() const throw(){
+			return ("Error, element not found");
+		}
+};
+
+template <typename T>
+typename T::iterator easyfind(T& container, int entier)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), entier);
+	if (it == container.end())
+		throw notfound();
+	return(it); 
+}
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,37 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: isouaidi <isouaidi@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/23 19:02:58 by isouaidi          #+#    #+#             */
-/*   Updated: 2024/07/23 19:29:19 by isouaidi         ###   ########.fr       */
+/*   Created: 2024/07/24 15:40:48 by isouaidi          #+#    #+#             */
+/*   Updated: 2024/07/24 15:40:49 by isouaidi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"easyfind.hpp"
-#include<iostream>
-#include<vector>
-
-int main(){
-	std::vector<int> vec;
-
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
-	vec.push_back(4);
-
-	// for (size_t i = 0; i < 4; i++)
-	// 	std::cout << " "<< vec[i] << std::endl;
-	
-	// std::cout << std::endl;
-
-	try
-	{
-		std::vector<int>::iterator it = easyfind(vec, 3);
-		std::cout << "Found = " << *it << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	
-	
-}
