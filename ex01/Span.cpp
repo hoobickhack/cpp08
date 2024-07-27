9fb58e831bb7de1058b8954dee25d803a4db68b0
ilyes <leilyesdu06300@gmail.com>
2024-07-27 18:05:08 +0200

    push for school

---
+++ ex01/Span.cpp
@@ -0,0 +1,7 @@
+
+#include "Span.hpp"
+
+
+Span::Span():N(0){};
+
+Span:: ~Span(){};
--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -11,9 +11,43 @@
 /* ************************************************************************** */
 
 # pragma once
-
-template <typename T>
+#include <iostream>
+#include <exception>
+#include <vector>
 
 class Span {
-	
+    private:
+
+    std::vector<int> span;
+    unsigned int N; 
+
+    public:
+    Span();
+    Span(unsigned int N);
+    Span(const Span &instance);
+    Span &operator=(const Span &instance);
+    ~Span();
+
+    void addnumber(int n);
+
+    int shortestSpan();
+    int longestSpan();
+
+    void printSpan();
+
+    class maxN : public std::exception{
+        public : 
+        const char *what() const throw()
+        {
+            return ("Span is full\n");
+        }
+    };
+
+    class minN : public std::exception{
+        public : 
+        const char *what() const throw()
+        {
+            return ("no distance found\n");
+        }
+    }; 
 }; 
